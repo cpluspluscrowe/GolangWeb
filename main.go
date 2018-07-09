ef266d87134269d80d40f003b9e9d05eb2c56a46
Chad Crowe <ccrowe@linkedin.com>
2018-07-08 20:24:38 -0500

    Added 404 page

--- main.go
+++ main.go
@@ -4,6 +4,7 @@ import (
 	"fmt"
 	"github.com/gorilla/mux"
 	"net/http"
+	"os"
 	"time"
 )
 
@@ -28,12 +29,18 @@ func pageHandler(w http.ResponseWriter, r *http.Request) {
 	vars := mux.Vars(r)
 	pageId := vars["id"]
 	fileName := "files/" + pageId + ".html"
+	_, err := os.Stat(fileName)
+	if err != nil {
+		fileName = "files/404.html"
+	}
 	http.ServeFile(w, r, fileName)
 }
 
 func main() {
 	rtr := mux.NewRouter()
 	rtr.HandleFunc("/pages/{id:[0-9]+}", pageHandler)
+	rtr.HandleFunc("/homepage", pageHandler)
+	rtr.HandleFunc("/contact", pageHandler)
 	http.Handle("/", rtr)
 	http.ListenAndServe(Port, nil)
 }
